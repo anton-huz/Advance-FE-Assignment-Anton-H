95859189a3f4100ab244f32c8094dd0b04594ec8
Anton Huz <anton.huz@outlook.com>
2026-02-19 20:18:52 +0200

    chore: Fixed code style

--- src/views/accounts/components/AccountDetail.tsx
+++ src/views/accounts/components/AccountDetail.tsx
@@ -2,14 +2,13 @@
 
 import React from 'react';
 
-import {Account} from '@/domain/Account';
 import {Typography} from '@mui/material';
 import useFetchAccounts from '@/hooks/useFetchAccounts';
 import {useGlobalSearch} from '@core/hooks/useGlobalSearch';
 import {FlexxTable} from '@components/FlexxTable/FlexxTable';
 import useAccountsDashboardTable from '@views/accounts/hooks/useAccountsDashboardTable';
 
-const AccountDetail: React.FC<{account?: Account}> = ({account}) => {
+const AccountDetail: React.FC = () => {
   const {searchQuery} = useGlobalSearch();
   const {data, isLoading, isError} = useFetchAccounts({searchQuery});
   const {columns, rows, DrawerHolder} = useAccountsDashboardTable(data);
